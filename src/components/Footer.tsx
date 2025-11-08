0830436ee6f8142e1a923c4fea3e0c46d54dac15
younguk <0622ook@gmail.com>
2025-11-08 22:39:39 +0900

    add3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer" style={{ marginTop: '30px' }}>
       <div className="container">
-        <span className="text-muted">강의 출처 : 인프런 - jQuery로 구현하는 NFT 마켓플레이스 (이더리움, 폴리곤)</span>
+        <span className="text-muted">강의 출처 : 인프런 - jQuery로 구현하는 NFT 마켓플레이스 (이더리움, 폴리곤)- 김영욱 92212788</span>
       </div>
     </footer>
   );
